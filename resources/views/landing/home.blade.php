0b55d0006454ddb19375c8e578207b65d2de8d3d
Yogapratama1109 <yoga110011@gmail.com>
2023-01-10 20:21:44 +0700

    update

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -6,27 +6,25 @@
 @endsection
 
 @section('content')
+    {{-- Navbar --}}
 
-     {{-- Navbar --}}
-
-     <nav class="navbar navbar-expand-md navbar-light fixed-top bg-primary">
+    <nav class="navbar navbar-expand-md navbar-light sticky-top">
         <div class="container">
             <a class="navbar-brand" id="#">
-                <img src="" alt="" class="logoFan">
+                <img src="" alt="LogoWk" class="logoFan">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-1 mb-lg-0 ">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" >Beranda</a>
+                        <a class="nav-link active" href="#">Beranda</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#" >Program Beasiswa</a>
+                        <a class="nav-link" href="#">Program Beasiswa</a>
                     </li>
 
                     <li class="nav-item">
@@ -47,23 +45,28 @@
         </div>
     </nav>
 
-    <div class="d-flex flex-row pt-5 mt-5">
-        <div class="col-6 pt-2 mt-5 px-5">
-            <div class="container m-5">
-                <h1 class="banner-title text-primary">Program Beasiswa</h1>
-                <h1 class="banner-title">SMK Wikrama Bogor</h1>
-                <p class="card-text">
-                    Ilmu yang Amaliah, Amal yang Ilmiah, Akhlakul Karimah
+    <div class="container mt-5">
+        <div class="d-flex flex-column-reverse flex-md-row align-items-center w-100">
+            <div class="w-100 w-md-50 mt-3 mt-md-0 text-center text-md-start">
+                <h1 class="text-primary">Program Beasiswa</h1>
+                <h1>SMK Wikrama Bogor</h1>
+                <p class="card-text pe-0 pe-md-5 text-muted">
+                    Beasiswa adalah bantuan biaya khususnya dalam bidang pendidikan sehingga seorang pelajar bisa mengikuti kegiatan belajar dengan biaya yang lebih ringan. Karena bersifat bantuan, besaran beasiswa yang diberikan ke penerima berbeda-beda, dapat berupa beasiswa penuh (full scholarship), beasiswa sebagian (partial scholarship) atau bantuan fasilitas tertentu saja yang menunjang pembelajaran.
                 </p>
-
+                <div class="d-flex justify-content-center justify-content-md-start mt-4">
+                    <button class="btn btn-donasi btn-primary me-1">
+                        <a href="/form/submission" class="text-white text-decoration-none">Donasi</a>
+                    </button>
+                    <button class="btn btn-donasi-outline btn-outline-primary">
+                        <a href="/form/submission" class="text-decoration-none">Ajukan Beasiswa</a>
+                    </button>
+                </div>
+            </div>
+            <div class="w-100 w-md-50">
+                <img src="{{ asset('assets/img/animasi.png') }}" class="w-100" alt="" />
             </div>
         </div>
-        <div class="col-6 pt-5 ">
-            <img src="{{ asset('assets/img/animasi.png') }}" class="banner-img" alt="" />
-          </div>
     </div>
-   
-    
 @endsection
 
 @section('js')
